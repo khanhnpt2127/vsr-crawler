a8f58a79d7ac56d3eca85d1da97d0f2fc18140d8
TK Nguyen <khanh.npt2609@gmail.com>
2020-02-04 17:45:01 +0100

    first crawler content

--- vsr-crawler.ConsoleApp/Program.cs
+++ vsr-crawler.ConsoleApp/Program.cs
@@ -4,6 +4,35 @@ namespace vsr_crawler.ConsoleApp
 {
     class Program
     {
+
+        static string GetChildValueByInnerHtmlWithSpecialContains(HtmlNode htmlSources) {
+            string res = "";
+
+            if(!string.IsNullOrWhiteSpace(htmlSources.InnerHtml))
+            if(htmlSources.InnerHtml.Contains("Room")) {
+                System.Console.WriteLine((htmlSources.InnerHtml));
+                res = htmlSources.InnerHtml;
+            }
+            return res;
+        }
+
+
+        static string GetChildValueByAttribute(HtmlNode htmlSources) {
+            System.Console.WriteLine(htmlSources.Attributes["src"].Value);
+            return htmlSources.Attributes["src"].Value;
+        }
+
+        static string GetChildValueByInnerHtml(HtmlNode htmlSources) {
+            string res = "";
+
+            if(!string.IsNullOrWhiteSpace(htmlSources.InnerHtml)) {
+                System.Console.WriteLine((htmlSources.InnerHtml));
+                res = htmlSources.InnerHtml;
+            }
+            
+            return res;
+        }
+
         static void Main(string[] args)
         {
 
@@ -16,16 +45,33 @@ namespace vsr_crawler.ConsoleApp
             
                 var nodes = document.DocumentNode.SelectNodes("//main//div[@class='mitarbeiter']");
 
-
                 foreach(HtmlNode node in nodes) {
-                    var imgs = node.SelectNodes(".//img");
-                    
 
-                    if(imgs != null) {
-                        foreach(var img in imgs) {
-                            Console.WriteLine(img.Attributes["src"].Value);
-                        }
+                    //info: get Room
+
+                    var selectedMainRoomNodes = node.SelectNodes(".//p");
+                    //System.Console.WriteLine(h3[h3.Count-1].InnerHtml);
+                    
+                    if(selectedMainRoomNodes != null)
+                    foreach(var selectedNode in selectedMainRoomNodes) {
+                        GetChildValueByInnerHtmlWithSpecialContains(selectedNode);
                     }
+
+                    // info: get Img
+                    var selectedMainImgNodes = node.SelectNodes(".//img");
+
+                    if(selectedMainImgNodes != null)
+                    foreach(var selectedNode in selectedMainImgNodes) {
+                        GetChildValueByAttribute(selectedNode);
+                    }
+
+                    // info: get Name
+
+                    var selectedMainNameNodes = node.SelectNodes(".//h3");
+
+                    if(selectedMainNameNodes != null)
+                        foreach(var selectedNode in selectedMainNameNodes) 
+                            GetChildValueByInnerHtml(selectedNode);
                 }
                 /*
                 foreach(HtmlNode node in nodes) 
