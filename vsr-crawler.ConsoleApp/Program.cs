9dd8c1f5280a1618bef41642bc4c957205234fb0
TK Nguyen <khanh.npt2609@gmail.com>
2020-02-04 14:59:02 +0100

    first implementation of get the img attr in html page

--- vsr-crawler.ConsoleApp/Program.cs
+++ vsr-crawler.ConsoleApp/Program.cs
@@ -6,15 +6,48 @@ namespace vsr_crawler.ConsoleApp
     {
         static void Main(string[] args)
         {
-            HtmlWeb web = new HtmlWeb();  
-            HtmlDocument document = web.Load("http://www.tu-chemnitz.de/informatik/HomePages/Medieninformatik/team.php.en"); 
-        
-			var nodes = document.DocumentNode.SelectNodes("//main");
-            foreach(var node in nodes) {
-                Console.WriteLine(node);
-            }
 
-            Console.ReadLine();
+            System.Console.WriteLine("App is running ... ");
+
+            try
+            {
+                HtmlWeb web = new HtmlWeb();  
+                HtmlDocument document = web.Load("http://www.tu-chemnitz.de/informatik/HomePages/Medieninformatik/team.php.en"); 
+            
+                var nodes = document.DocumentNode.SelectNodes("//main//div[@class='mitarbeiter']");
+
+
+                foreach(HtmlNode node in nodes) {
+                    var imgs = node.SelectNodes(".//img");
+                    
+
+                    if(imgs != null) {
+                        foreach(var img in imgs) {
+                            Console.WriteLine(img.Attributes["src"].Value);
+                        }
+                    }
+                }
+                /*
+                foreach(HtmlNode node in nodes) 
+                {
+                   var vcard = node.SelectNodes(".//address"); 
+                   if(vcard != null) {
+                   foreach(HtmlNode member in vcard) {
+                       Console.WriteLine(member.SelectSingleNode("//img").Attributes["src"].Value);
+                   } }
+                }
+                */
+
+                Console.ReadLine();
+            }
+            catch(Exception ex) {
+                Console.WriteLine(ex);
+            }
+            finally
+            {
+                Console.WriteLine("Press any key to exit ...");
+                Console.ReadLine();
+            }
         }
     }
 }
